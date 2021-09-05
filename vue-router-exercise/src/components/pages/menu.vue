730990bdb43c7a7c8a4340367cd4c05be683c3c0
zamhsu <41725143+zamhsu@users.noreply.github.com>
2021-09-05 19:45:55 +0800

    新增10-79練習結果

--- vue-router-exercise/src/components/pages/menu.vue
+++ vue-router-exercise/src/components/pages/menu.vue
@@ -10,6 +10,21 @@
       <li class="nav-item">
         <router-link class="nav-link" to="/page/child/foobar">卡片 3</router-link>
       </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link" @click.prevent="routerPush()">到卡片2</a>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link" @click.prevent="routerBack()">回到上一頁</a>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link" @click.prevent="routerForward()">下一頁</a>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link" @click.prevent="routerGo()">當前頁面上或下幾頁</a>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link" @click.prevent="routerReplace()">刪除當前頁面紀錄跳到卡片3</a>
+      </li>
     </ul>
   </div>
 </template>
@@ -19,5 +34,29 @@ export default {
   data() {
     return {};
   },
+  methods: {
+    routerPush() {
+      // 到指定路徑
+      this.$router.push('/page/child2');
+    },
+    routerBack() {
+      // 上一頁
+      this.$router.back();
+    },
+    routerForward() {
+      // 下一頁
+      this.$router.forward();
+    },
+    routerGo() {
+      // 當前頁面上或下幾頁
+      // -1同back
+      // 1同forward
+      this.$router.go(-2);
+    },
+    routerReplace() {
+      // 將當前頁面替換為指定路徑，且替換前的頁面不會被記錄
+      this.$router.replace('/page/child/foobar');
+    }
+  }
 };
 </script>
