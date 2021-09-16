9ac57867378dd704396f373d65c39c967dc8dea8
zamhsu <sam2009mn32@gmail.com>
2021-09-16 21:11:09 +0800

    新增帶入電商專屬API

--- vue-ecshop-exercise/src/main.js
+++ vue-ecshop-exercise/src/main.js
@@ -1,4 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 
-createApp(App).mount('#app')
+import App from './App.vue';
+
+createApp(App)
+    .use(VueAxios, axios)
+    .mount('#app');
