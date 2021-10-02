d13d744566446692945b0b860bbc157bd7dea10a
zamhsu <41725143+zamhsu@users.noreply.github.com>
2021-10-02 15:37:46 +0800

    修改側選單內容

--- vue-ecshop-exercise/src/components/Navbar.vue
+++ vue-ecshop-exercise/src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
     <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#"
-      >Company name</a
+      >EC Shop</a
     >
     <button
       class="navbar-toggler position-absolute d-md-none collapsed"
--- vue-ecshop-exercise/src/components/Sidebar.vue
+++ vue-ecshop-exercise/src/components/Sidebar.vue
@@ -4,41 +4,42 @@
     class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse"
   >
     <div class="sidebar-sticky pt-3">
-      <ul class="nav flex-column">
+      <h6
+        class="
+          sidebar-heading
+          d-flex
+          justify-content-between
+          align-items-center
+          px-3
+          mt-4
+          mb-1
+          text-muted
+        "
+      >
+        <span>管理員</span>
+        <a
+          class="d-flex align-items-center text-muted"
+          href="#"
+          aria-label="Add a new report"
+        >
+          <span data-feather="plus-circle"></span>
+        </a>
+      </h6>
+      <ul class="nav flex-column mb-2">
         <li class="nav-item">
-          <a class="nav-link active" href="#">
-            <span data-feather="home"></span>
-            Dashboard <span class="sr-only">(current)</span>
-          </a>
-        </li>
-        <li class="nav-item">
+          <router-link to="/admin/products" class="nav-link active">
+            <i class="fas fa-gift"></i>
+            產品列表
+          </router-link>
           <a class="nav-link" href="#">
-            <span data-feather="file"></span>
-            Orders
+            <span data-feather="file-text"></span>
+            <i class="fas fa-list-ul"></i>
+            訂單列表
           </a>
-        </li>
-        <li class="nav-item">
           <a class="nav-link" href="#">
-            <span data-feather="shopping-cart"></span>
-            Products
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">
-            <span data-feather="users"></span>
-            Customers
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">
-            <span data-feather="bar-chart-2"></span>
-            Reports
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">
-            <span data-feather="layers"></span>
-            Integrations
+            <span data-feather="file-text"></span>
+            <i class="fas fa-ticket-alt"></i>
+            優惠券
           </a>
         </li>
       </ul>
@@ -55,7 +56,7 @@
           text-muted
         "
       >
-        <span>Saved reports</span>
+        <span>模擬功能</span>
         <a
           class="d-flex align-items-center text-muted"
           href="#"
@@ -68,25 +69,8 @@
         <li class="nav-item">
           <a class="nav-link" href="#">
             <span data-feather="file-text"></span>
-            Current month
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">
-            <span data-feather="file-text"></span>
-            Last quarter
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">
-            <span data-feather="file-text"></span>
-            Social engagement
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">
-            <span data-feather="file-text"></span>
-            Year-end sale
+            <i class="fas fa-shopping-cart"></i>
+            模擬訂單
           </a>
         </li>
       </ul>
