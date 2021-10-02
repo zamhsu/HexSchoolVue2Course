f1cbe396997674cb6d195972d57055a51b119c5d
zamhsu <41725143+zamhsu@users.noreply.github.com>
2021-10-02 20:24:38 +0800

    模擬訂單加入顯示商品詳細資料

--- vue-ecshop-exercise/src/apis/hexSchoolUser.js
+++ vue-ecshop-exercise/src/apis/hexSchoolUser.js
@@ -1,5 +1,8 @@
 export default {
     getProductsUrl(page = 1) {
         return `${process.env.VUE_APP_API_PATH}/api/${process.env.VUE_APP_CUSTOM_PATH}/products?page=${page}`;
+    },
+    getSingleProductUrl(id) {
+        return `${process.env.VUE_APP_API_PATH}/api/${process.env.VUE_APP_CUSTOM_PATH}/product/${id}`;
     }
 }
--- vue-ecshop-exercise/src/components/pages/CustomerOrder.vue
+++ vue-ecshop-exercise/src/components/pages/CustomerOrder.vue
@@ -2,7 +2,7 @@
   <div>
     <loading v-model:active="isLoading" />
     <div class="row mt-5">
-      <div class="col-md-4 mb-4" v-for="item in products" :key="item.no">
+      <div class="col-md-4 mb-4" v-for="item in products" :key="item.id">
         <div class="card border-0 shadow-sm">
           <div
             style="
@@ -10,29 +10,109 @@
               background-size: cover;
               background-position: center;
             "
-            :style="{backgroundImage: `url(${item.imageUrl})`}"
+            :style="{ backgroundImage: `url(${item.imageUrl})` }"
           ></div>
           <div class="card-body">
-            <span class="badge badge-secondary float-right ml-2">{{item.category}}</span>
+            <span class="badge badge-secondary float-right ml-2">{{
+              item.category
+            }}</span>
             <h5 class="card-title">
-              <a href="#" class="text-dark">{{item.title}}</a>
+              <a href="#" class="text-dark">{{ item.title }}</a>
             </h5>
-            <p class="card-text">{{item.content}}</p>
+            <p class="card-text">{{ item.content }}</p>
             <div class="d-flex justify-content-between align-items-baseline">
-              <div class="h5" v-if="!item.price">{{item.origin_price}} 元</div>
-              <del class="h6" v-if="item.price">原價 {{item.origin_price}} 元</del>
-              <div class="h5" v-if="item.price">現在只要 {{item.price}} 元</div>
+              <div class="h5" v-if="!item.price">
+                {{ item.origin_price }} 元
+              </div>
+              <del class="h6" v-if="item.price"
+                >原價 {{ item.origin_price }} 元</del
+              >
+              <div class="h5" v-if="item.price">
+                現在只要 {{ item.price }} 元
+              </div>
             </div>
           </div>
           <div class="card-footer d-flex">
-            <button type="button" class="btn btn-outline-secondary btn-sm">
-              <i class="fas fa-search"></i>
+            <button
+              type="button"
+              class="btn btn-outline-secondary btn-sm"
+              @click="getProduct(item.id)"
+            >
+              <i
+                class="fas fa-spinner fa-spin"
+                v-if="status.loadingItem === item.id"
+              ></i>
               查看更多
             </button>
             <button type="button" class="btn btn-outline-danger btn-sm ml-auto">
-              <i class="fas fa-cart-plus"></i>
+              <i class="fas fa-spinner fa-spin"></i>
               加到購物車
             </button>
+          </div>
+        </div>
+      </div>
+
+      <!-- product modal -->
+      <div
+        class="modal fade"
+        id="productModal"
+        tabindex="-1"
+        role="dialog"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">
+                {{ product.title }}
+              </h5>
+              <button
+                type="button"
+                class="close"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <img :src="product.imageUrl" class="img-fluid" alt="" />
+              <blockquote class="blockquote mt-3">
+                <p class="mb-0 allow-break">{{ product.content }}</p>
+                <footer class="blockquote-footer text-right allow-break">
+                  {{ product.description }}
+                </footer>
+              </blockquote>
+              <div class="d-flex justify-content-between align-items-baseline">
+                <div class="h4" v-if="!product.price">
+                  {{ product.origin_price }} 元
+                </div>
+                <del class="h6" v-if="product.price"
+                  >原價 {{ product.origin_price }} 元</del
+                >
+                <div class="h4" v-if="product.price">
+                  現在只要 {{ product.price }} 元
+                </div>
+              </div>
+              <select name="" class="form-control mt-3" v-model="product.num">
+                <option :value="num" v-for="num in 10" :key="num">
+                  選購 {{ num }} {{ product.unit }}
+                </option>
+              </select>
+            </div>
+            <div class="modal-footer">
+              <div class="text-muted text-nowrap mr-3">
+                小計 <strong>{{ product.num * product.price }}</strong> 元
+              </div>
+              <button type="button" class="btn btn-primary">
+                <i
+                  class="fas fa-spinner fa-spin"
+                  v-if="product.id === status.loadingItem"
+                ></i>
+                加到購物車
+              </button>
+            </div>
           </div>
         </div>
       </div>
@@ -41,12 +121,18 @@
 </template>
 
 <script>
+import $ from "jquery";
+
 import userApiUrl from "../../apis/hexSchoolUser";
 
 export default {
   data() {
     return {
       products: [],
+      product: {},
+      status: {
+        loadingItem: "",
+      },
       isLoading: false,
     };
   },
@@ -64,9 +150,28 @@ export default {
         self.isLoading = false;
       });
     },
+    getProduct(id) {
+      this.status.loadingItem = id;
+
+      let self = this;
+      const api = userApiUrl.getSingleProductUrl(id);
+      this.$http.get(api).then((response) => {
+        // console.log(response.data);
+        self.product = response.data.product;
+        self.product.num = 1; // 設定num預設1，否則數量下拉選單會是null
+        $("#productModal").modal("show");
+        self.status.loadingItem = "";
+      });
+    },
   },
   created() {
     this.getProducts();
   },
 };
 </script>
+
+<style scoped>
+.allow-break {
+  white-space: pre-line;
+}
+</style>
